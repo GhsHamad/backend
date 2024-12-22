f52d5d54cedf1e67be4b64ded501b368a6ab0874
GhsHamad <ghshamad314@gmail.com>
2024-12-23 03:30:28 +0500

    cors

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ connectDB().catch((err) => {
 
 // App setup
 const app = express();
-app.use(cors({ origin: "*" }));
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(json());
 
 // Routes setup
